28a694d6c8a95735bcdeff44644d8cb3955c4f68
Ivan Camacho <Ivancamacho2578@gmail.com>
2022-10-19 14:19:12 -0700

    Test

--- dvg/frontend/src/main.js
+++ dvg/frontend/src/main.js
@@ -1,5 +1,24 @@
-import { createApp, VueElement } from 'vue'
+import { createApp,h } from 'vue'
 import App from './App.vue'
-import router from '@/router'
+import router from './router'
+import { ApolloClient } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
 
-createApp(App).use(router).mount('#app')
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+    //cache,
+    uri: 'http://127.0.0.1:8000/graphql'
+})
+
+const apolloProvider = createApolloProvider({
+    defaultClient: apolloClient,
+})
+
+const app = createApp({
+    render: () => h(App),
+}).use(router)
+
+app.user(router)
+app.user(apolloProvider)
+app.mount('#app')
